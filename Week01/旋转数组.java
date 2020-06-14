caea8def69e25196c4ffc2762efdd6269c3fedc7
benjiazhen <benjiazhen@163.com>
2020-06-14 10:35:33 +0800

    旋转数组.java

---
+++ Week01/旋转数组.java
@@ -0,0 +1,51 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        method(nums,  k);
+    }
+    //1.暴力破解
+    // public void method(int[] nums,int k)
+    // {
+    //     for(int i=0;i<k;i++)
+    //     {
+    //         int previous = nums[nums.length-1];
+    //         for(int j = 0 ;j< nums.length;j++)
+    //         {
+    //             int tmp = nums[j];
+    //             nums[j]=previous;
+    //             previous = tmp;
+    //         }
+    //     }
+    // }
+//2.向右移动k+额外存储空间
+//         public void method(int[] nums,int k)
+//     {
+//         int[] a = new int[nums.length];
+// for (int i = 0; i < nums.length; i++) {
+//             a[(i + k) % nums.length] = nums[i];
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             nums[i] = a[i];
+//         }
+//     }
+
+//三次翻转
+        public void method(int[] nums,int k)
+    {
+
+        k%=nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    public void reverse(int[] nums,int start,int end)
+    {
+        while(start < end)
+        {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start ++;
+            end --; 
+        }
+    }
+}
