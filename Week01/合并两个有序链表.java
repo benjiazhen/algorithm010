bcfa590da0e30a8aa8ec8456c440e3146e110c0d
benjiazhen <benjiazhen@163.com>
2020-06-14 18:24:50 +0800

    合并连个有序链表

--- Week01/合并两个有序链表.java
+++ Week01/合并两个有序链表.java
@@ -10,52 +10,52 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return contact(l1,l2);
+        return iteration(l1,l2);
     }
     //1.迭代
     //时间复杂度:O(m+n)
     //空间复杂度:O(1)
-    // public ListNode contact(ListNode l1, ListNode l2)
-    // {
-    //     ListNode newLink = new ListNode(-1);
-    //     ListNode pre = newLink;
-    //     while(l1 != null && l2 != null)
-    //     {
-    //         if(l1.val <= l2.val)
-    //         {
-    //             pre.next = l1;
-    //             l1 = l1.next;
-    //         }else
-    //         {
-    //             pre.next = l2;
-    //             l2 = l2.next;
-    //         }
-    //         pre = pre.next;
-    //     }
-    //     pre.next = l1 == null ? l2 : l1;
-    //     return newLink.next;
+    public ListNode iteration(ListNode l1, ListNode l2)
+    {
+        ListNode newLink = new ListNode(-1);
+        ListNode pre = newLink;
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val <= l2.val)
+            {
+                pre.next = l1;
+                l1 = l1.next;
+            }else
+            {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = l1 == null ? l2 : l1;
+        return newLink.next;
 
-    // }
+    }
     //2.递归
     //时间复杂度:O(m+n)
     //空间复杂度:O(m+n)
-    public ListNode contact(ListNode list1,ListNode list2)
+    public ListNode recurse(ListNode list1,ListNode list2)
     {
         if(list1 == null)
         {
-            return null;
+            return list2;
         }
-        if(list2 == null)
+        else if(list2 == null)
         {
-            return null;
+            return list1;
         }
-        if(list1.val <= list2.val)
+        else if(list1.val <= list2.val)
         {
-            list1.next = contact(list1.next,list2);
+            list1.next = recurse(list1.next,list2);
             return list1;
         }else
         {
-            list2.next = contact(list1,list2.next);
+            list2.next = recurse(list1,list2.next);
             return list2;
         }
     }
